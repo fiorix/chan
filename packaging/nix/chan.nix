7191dcd21d22858042a878a6118599a4a1157639
Alexandre Fiori <fiorix@gmail.com>
2026-09-23 04:04:05 +0000

    chore(release): 0.100.0-rc1

--- packaging/nix/chan.nix
+++ packaging/nix/chan.nix
@@ -26,12 +26,12 @@ rustPlatform.buildRustPackage (finalAttrs: {
   pname = "chan";
   inherit version src;
 
-  cargoHash = "sha256-bzjoKfXwCvVP72FYv9Srj8qr6kvItiFtWGByX7NL/gs=";
+  cargoHash = "sha256-ykf2B+GD/wYVziJUNWIHA+KDljiR0uwSCmo/pmRY6Lc=";
 
   npmDeps = fetchNpmDeps {
     name = "${finalAttrs.pname}-${finalAttrs.version}-npm-deps";
     src = "${finalAttrs.src}/web";
-    hash = "sha256-tocOO9q54aIRq0MZaFHxb1S7gcS+y7CGYllAGBi1Jo8=";
+    hash = "sha256-cF2aLtIEuJ+dB2aCHhRVcSIw/6wNCgs27U3IW15O620=";
   };
   npmRoot = "web";
 
